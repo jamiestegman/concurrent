20867b9c3587ffcf71955d243442ba2abd650590
Jamie Stegman <hello@jamiestegman.com>
2023-07-15 21:36:31 +1000

    Initial setup commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import type { AppProps } from "next/app";
+
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <style jsx global>{`
+                html {
+                    font-family: ${inter.style.fontFamily};
+                }
+            `}</style>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
